2f57cabc205fc165ccceed37c52d1d003b196339
achechi15 <142111179+achechi15@users.noreply.github.com>
2023-11-24 12:41:02 +0100

    Implementation

---
+++ euclidean.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int euclidean(int a, int b)
+{
+    return b == 0? a : euclidean(b, a % b);
+}
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << euclidean(a, b) << endl;
+}
